b121f53caae23d1061896587a8696299ec52fab3
Aydot <ayomidotzee@gmail.com>
2023-03-03 19:44:46 +0100

    Flag sixe changed

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
         <div className='flex justify-between'>
             <div className='flex gap-6'>
                 <div className='flex gap-3'>
-                    <img src={countries[0].flag} className='w-[40px]'/>
+                    <img src={countries[0].flag} className='w-[40px] h-[30px]'/>
                     <p className='text-[1.2rem]'>{countries[0].name}</p>
                 </div>
                 <p className='flex text-green-600 text-[1.1rem] items-center'>Change <span><BiChevronRight /></span></p>
